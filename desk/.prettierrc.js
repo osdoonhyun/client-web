1607f558db65793bff17830decbb44d3ace2096e
Seungjin Kim <seungjin429@gmail.com>
2023-04-12 16:15:07 +0900

    [QCF-6] prettierrc 주석

--- desk/.prettierrc.js
+++ desk/.prettierrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'none',
-  arrowParens: 'avoid',
-  semi: false,
-  printWidth: 90
+  bracketSpacing: false, // 객체 리터럴에서 괄호에 공백 삽입 여부
+  jsxBracketSameLine: false, // JSX의 마지막 `>`를 다음 줄로 내릴지 여부
+  singleQuote: true, // single 쿼테이션 사용 여부
+  trailingComma: 'all', // 여러 줄을 사용할 때, 후행 콤마 사용 방식
+  arrowParens: 'avoid', // 화살표 함수 괄호 사용 방식
+  semi: false, // 세미콜론 사용 여부
+  printWidth: 90, //  줄 바꿈 할 폭 길이
+  tabWidth: 2 // 탭 너비
 }
