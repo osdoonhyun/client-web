a397615634a11561dee7bc49a0037e44d4be5142
yrk721 <yoorae0721@naver.com>
2023-04-14 19:47:18 +0900

    [QCF-19] 공통 Layout 기본구조 구현

---
+++ desk/src/commons/libraries/layout/index.tsx
@@ -0,0 +1,11 @@
+import LayoutFooter from './footer/LayoutFooter.container'
+import LayoutHeader from './header/LayoutHeader.container'
+
+export default function Layout() {
+  return (
+    <>
+      <LayoutHeader />
+      <LayoutFooter />
+    </>
+  )
+}
