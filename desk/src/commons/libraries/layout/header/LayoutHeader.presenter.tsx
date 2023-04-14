7deb226780d659300bc1836dc3fbcb6bcc50b6ab
yrk721 <yoorae0721@naver.com>
2023-04-14 19:46:49 +0900

    [QCF-19] Layout Header UI 구현 - darkmode 적용

---
+++ desk/src/commons/libraries/layout/header/LayoutHeader.presenter.tsx
@@ -0,0 +1,108 @@
+import { ReactNode } from 'react'
+import {
+  Box,
+  Text,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from '@chakra-ui/react'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+
+const Logo = (props: any) => {
+  return (
+    <>
+      <Box mb={4} ml={5}>
+        <Text as="b" fontSize="30px" color="dPimary">
+          dechaive
+          <Text
+            as="b"
+            fontSize="50px"
+            color="dPimary"
+            marginLeft="5px"
+            marginBottom="50px">
+            .
+          </Text>
+        </Text>
+      </Box>
+    </>
+  )
+}
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}>
+    {children}
+  </Link>
+)
+
+export default function LayoutHeaderUI() {
+  const { colorMode, toggleColorMode } = useColorMode()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Box bg={useColorModeValue('dGray.light', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Logo />
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={4}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}>
+                  <Avatar
+                    mr={2}
+                    size={'sm'}
+                    src={'https://avatars.dicebear.com/api/male/username.svg'}
+                  />
+                </MenuButton>
+                <MenuList alignItems={'center'}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={'xl'}
+                      src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>닉네임</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>마이페이지</MenuItem>
+                  <MenuItem>로그아웃</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  )
+}
