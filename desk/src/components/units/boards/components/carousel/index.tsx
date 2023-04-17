68b80f9f4af8dacbeb380fb8a6f35777844c01e6
Seungjin Kim <seungjin429@gmail.com>
2023-04-17 22:25:31 +0900

    [QCF-32] Carousel UI 개발

---
+++ desk/src/components/units/boards/components/carousel/index.tsx
@@ -0,0 +1,106 @@
+import { Box, IconButton, Image, useBreakpointValue } from '@chakra-ui/react'
+import { useState } from 'react'
+import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi'
+import Slider from 'react-slick'
+
+const settings = {
+  dots: true,
+  arrows: false,
+  fade: true,
+  infinite: true,
+  autoplay: true,
+  speed: 500,
+  autoplaySpeed: 5000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+}
+
+export default function Carousel() {
+  const [slider, setSlider] = useState<Slider | null>(null)
+
+  const top = useBreakpointValue({ base: '90%', md: '50%' })
+  const side = useBreakpointValue({ base: '30%', md: '20px' })
+
+  // Text Data
+  const cards = [
+    {
+      image:
+        'https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+    },
+    {
+      image:
+        'https://images.unsplash.com/photo-1438183972690-6d4658e3290e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80',
+    },
+    {
+      image:
+        'https://images.unsplash.com/photo-1507237998874-b4d52d1dd655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+    },
+  ]
+
+  return (
+    <Box
+      position={'relative'}
+      height={{ base: '500px', md: '600px' }}
+      width={'full'}
+      overflow={'hidden'}>
+      <link
+        rel="stylesheet"
+        type="text/css"
+        charSet="UTF-8"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+      />
+      {/* Left Icon */}
+      <IconButton
+        aria-label="left-arrow"
+        variant="ghost"
+        position="absolute"
+        left={side}
+        top={top}
+        transform={'translate(0%, -50%)'}
+        zIndex={2}
+        bgColor={'rgba(35, 35, 35, 0.3)'}
+        borderRadius={'full'}
+        onClick={() => slider?.slickPrev()}
+        _hover={{
+          bg: 'rgba(35, 35, 35, 0.5)',
+        }}>
+        <BiLeftArrowAlt size="20px" color="white" />
+      </IconButton>
+      {/* Right Icon */}
+      <IconButton
+        aria-label="right-arrow"
+        variant="ghost"
+        position="absolute"
+        right={side}
+        top={top}
+        transform={'translate(0%, -50%)'}
+        zIndex={2}
+        bgColor={'rgba(35, 35, 35, 0.3)'}
+        borderRadius={'full'}
+        onClick={() => slider?.slickNext()}
+        _hover={{
+          bg: 'rgba(35, 35, 35, 0.5)',
+        }}>
+        <BiRightArrowAlt size="20px" color="white" />
+      </IconButton>
+      {/* Slider */}
+      <Slider {...settings} ref={slider => setSlider(slider)}>
+        {cards.map((card, index) => (
+          <Image
+            key={index}
+            src={card.image}
+            height={'6xl'}
+            position="relative"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+            backgroundSize="cover"></Image>
+        ))}
+      </Slider>
+    </Box>
+  )
+}
--- desk/src/components/units/boards/detail/Detail.presenter.tsx
+++ desk/src/components/units/boards/detail/Detail.presenter.tsx
@@ -1,9 +1,11 @@
+import CaptionCarousel from '../components/carousel'
 import ProfileHeader from '../components/profileHeader'
 
 export default function BoardDetailUI() {
   return (
     <>
       <ProfileHeader />
+      <CaptionCarousel />
     </>
   )
 }
