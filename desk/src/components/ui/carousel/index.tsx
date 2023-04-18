aafbf44d68e02704ae419c8125d67c842a8acd45
Seungjin Kim <seungjin429@gmail.com>
2023-04-18 15:33:47 +0900

    [QCF-36] ant icon라이브러리 제거 후 아이콘 적용

--- desk/src/components/ui/carousel/index.tsx
+++ desk/src/components/ui/carousel/index.tsx
@@ -46,7 +46,6 @@ export default function Carousel() {
       <link
         rel="stylesheet"
         type="text/css"
-        charSet="UTF-8"
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
       />
       <link
--- desk/src/components/units/boards/components/tagWithCountState/index.tsx
+++ desk/src/components/units/boards/components/tagWithCountState/index.tsx
@@ -1,5 +1,5 @@
-import { CommentOutlined, HeartOutlined, RiseOutlined } from '@ant-design/icons'
 import { Flex, HStack, Text } from '@chakra-ui/react'
+import { AiOutlineComment, AiOutlineEye, AiOutlineHeart, IconName } from 'react-icons/ai'
 
 export default function TagWithCountState() {
   const testTags = ['개발자데스크셋업', '학생데스크셋업']
@@ -20,19 +20,19 @@ export default function TagWithCountState() {
         </HStack>
         <HStack spacing={6}>
           <HStack>
-            <HeartOutlined />
+            <AiOutlineHeart />
             <Text fontSize={14} fontWeight={600} color={'rgba(35, 35, 35, 0.7)'}>
               좋아요수
             </Text>
           </HStack>
           <HStack>
-            <RiseOutlined />
+            <AiOutlineEye />
             <Text fontSize={14} fontWeight={600} color={'rgba(35, 35, 35, 0.7)'}>
               조회수
             </Text>
           </HStack>
           <HStack>
-            <CommentOutlined />
+            <AiOutlineComment />
             <Text fontSize={14} fontWeight={600} color={'rgba(35, 35, 35, 0.7)'}>
               댓글수
             </Text>
