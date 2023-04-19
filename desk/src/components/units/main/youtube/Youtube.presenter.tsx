9daa84c240239e854fc0fea98d619e6f556c4d07
yrk721 <yoorae0721@naver.com>
2023-04-19 12:46:16 +0900

    [QCF-28] 유튜브 api 연결

--- desk/src/components/units/main/youtube/Youtube.presenter.tsx
+++ desk/src/components/units/main/youtube/Youtube.presenter.tsx
@@ -1,20 +1,37 @@
-import { Center } from '@chakra-ui/react'
-import YoutubeImageStyle from '@/src/components/ui/YoutubeImageStyle'
+import { Box, Center, Flex, Spinner, Text, useColorModeValue } from '@chakra-ui/react'
 import CategoryHeader from '../categoryHeader/CategoryHeader.container'
+import YoutubeImageStyle from '@/src/components/ui/YoutubeImageStyle'
+import { TYoutube } from '@/src/commons/types/generated/types'
+import { FiEye } from 'react-icons/fi'
 
-export default function YoutubeUI() {
+export type YoutubeUIProps = {
+  youtubeData: TYoutube[]
+}
+
+export default function YoutubeUI({ youtubeData }: YoutubeUIProps) {
   const categoryTitle = '유튜브'
+  const isMoreVisible = false
 
   return (
     <>
-      <CategoryHeader categoryTitle={categoryTitle} />
-      <Center m={2}>
-        {/* 이미지 UI 구성을 위한 임시 Key값. 추후 api 연결 시 수정 예정 */}
-        {[1, 1, 1].map(num => (
-          <Center key={num} m={'10px'}>
-            <YoutubeImageStyle src={`/YoutubeTest${num}.jpeg`} alt={`test image${num}`} />
-          </Center>
-        ))}
+      <CategoryHeader categoryTitle={categoryTitle} isMoreVisible={isMoreVisible} />
+      <Center>
+        <Flex m={2} pl={'2px'} pr={'2px'} maxWidth="1080px" overflowX="auto">
+          {youtubeData.map((youtube: TYoutube, index: number) => (
+            <Box key={youtube.videoUrl} m={2} flexShrink={0}>
+              <YoutubeImageStyle src={youtube.thumbnailUrl} alt={youtube.videoUrl} />
+              <Flex
+                justifyContent="end"
+                alignItems="center"
+                pr="3px"
+                color={useColorModeValue('dGray.dark', 'dGray.light')}
+                fontWeight="500">
+                <FiEye />
+                <Text ml="5px">{youtube.views} views</Text>
+              </Flex>
+            </Box>
+          ))}
+        </Flex>
       </Center>
     </>
   )
