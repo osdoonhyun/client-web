125d2f31d26f0b3e4825200f416f72dfaf72b21c
Seungjin Kim <seungjin429@gmail.com>
2023-04-27 00:10:40 +0900

    [QCF-30] 오타 수정

--- desk/src/components/ui/fileUpload/types.ts
+++ desk/src/components/ui/fileUpload/types.ts
@@ -6,7 +6,7 @@ export type FileUploadProps = {
   fileUrl: string | undefined
   width?: number | string
   height?: number | string
-  onChangeFile: (file: File, index: number) => void\
+  onChangeFile: (file: File, index: number) => void
   fileUploadRef?: React.RefObject<HTMLInputElement>
   children?: ReactNode
 }
