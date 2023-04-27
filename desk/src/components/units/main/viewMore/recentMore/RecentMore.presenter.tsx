d05712f5633dff17c79c8220f79bae3e938a918b
yrk721 <yoorae0721@naver.com>
2023-04-28 00:27:31 +0900

    [QCF-60] 최근게시물 더보기 fetchBoards api 연결

--- desk/src/components/units/main/viewMore/recentMore/RecentMore.presenter.tsx
+++ desk/src/components/units/main/viewMore/recentMore/RecentMore.presenter.tsx
@@ -1,4 +1,12 @@
-import { Center, Container, Flex, Text, useColorModeValue } from '@chakra-ui/react'
+import {
+  Avatar,
+  Box,
+  Center,
+  Container,
+  Flex,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react'
 import { RecentMoreUIProps } from './RecentMore.types'
 import MainImageStyle from '@/src/components/ui/mainImageStyle'
 import InfiniteScroll from 'react-infinite-scroller'
@@ -7,7 +15,7 @@ export default function RecentMoreUI(props: RecentMoreUIProps) {
   return (
     <>
       <Container
-        maxW="1200px"
+        maxW="1170px"
         h="900px"
         mt="50px"
         overflow="auto"
@@ -19,7 +27,7 @@ export default function RecentMoreUI(props: RecentMoreUIProps) {
           'scrollbar-width': 'none',
         }}>
         <Text
-          ml="40px"
+          ml="30px"
           fontSize="18pt"
           fontWeight="700"
           color={useColorModeValue('dGray.dark', 'dGray.light')}>
@@ -31,10 +39,39 @@ export default function RecentMoreUI(props: RecentMoreUIProps) {
           hasMore={true}
           useWindow={false}>
           <Flex flexWrap="wrap" justifyContent="center" m={2}>
-            {Array.from({ length: 100 }).map((_, index) => (
-              <Center key={index} m={'15px'}>
-                <MainImageStyle src={`/test1.jpeg`} alt={`test image1`} />
-              </Center>
+            {props.boards.map((board, index) => (
+              <Box
+                key={index}
+                m={'15px'}
+                maxW={{ base: '100%', md: '25%' }}
+                textAlign="center">
+                <MainImageStyle
+                  src={board.pictures.find(picture => picture.isMain)?.url ?? ''}
+                  alt={`test image${index}`}
+                />
+                <Text fontSize="13pt" fontWeight="bold" mt={2}>
+                  {board.title}
+                </Text>
+                <Center>
+                  <Text fontSize="11pt" mt={2}>
+                    <Avatar
+                      // size={'xs'}
+                      w="20px"
+                      h="20px"
+                      mr="5px"
+                    />
+                    {board.writer.nickName}
+                  </Text>
+                </Center>
+                {board.hashtags && (
+                  <Text fontSize="sm" mt={2}>
+                    {board.hashtags.map(hashtag => `#${hashtag.hashtag}`).join(' ')}
+                  </Text>
+                )}
+                <Text fontSize="sm" mt={1} color="dGray.dark">
+                  {`조회수: ${board.views}`}
+                </Text>
+              </Box>
             ))}
           </Flex>
         </InfiniteScroll>
