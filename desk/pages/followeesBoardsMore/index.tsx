c6b45e4c9d857439abb5b7832df40ce6c8a14092
yrk721 <yoorae0721@naver.com>
2023-04-27 12:46:27 +0900

    [QCF-54] 더보기 페이지 생성 / 메인페이지 카테고리 추가

---
+++ desk/pages/followeesBoardsMore/index.tsx
@@ -0,0 +1,9 @@
+import FolloweesBoardsMore from '@/src/components/units/main/viewMore/followeesBoardsMore/FolloweesBoardsMore.container'
+
+export default function FolloweesBoardsMorePage() {
+  return (
+    <>
+      <FolloweesBoardsMore />
+    </>
+  )
+}
