1c84dde27d10394fa1a36bf74032b158b3fe3525
osdoonhyun <skw1996@naver.com>
2023-05-01 00:00:13 +0900

    [QCF-65] 팔로워 / 팔로우 정보 바인딩

--- desk/src/components/units/user/components/followModal/index.tsx
+++ desk/src/components/units/user/components/followModal/index.tsx
@@ -22,53 +22,36 @@ import {
 import { MdPersonOutline } from 'react-icons/md'
 import { FollowModalProps } from './FollowModal.types'
 import { useAuth } from '@/src/commons/hooks/useAuth'
-
-// 테스트용 FOLLOWERS API 연결 후 삭제 예정
-const FOLLOWERS = [
-  {
-    id: 0,
-    name: 'Dan Abrahmov',
-    src: 'https://bit.ly/dan-abramov',
-    connections: 46,
-  },
-  {
-    id: 1,
-    name: 'Kola Tioluwani',
-    src: 'https://bit.ly/tioluwani-kolawole',
-    connections: 123,
-  },
-  {
-    id: 2,
-    name: 'Kent Dodds',
-    src: 'https://bit.ly/kent-c-dodds',
-    connections: 436,
-  },
-  {
-    id: 3,
-    name: 'Ryan Florence',
-    src: 'https://bit.ly/ryan-florence',
-    connections: 6,
-  },
-  {
-    id: 4,
-    name: 'Prosper Otemuyiwa',
-    src: 'https://bit.ly/prosper-baba',
-    connections: 1146,
-  },
-  {
-    id: 5,
-    name: 'Christian Nwamba',
-    src: 'https://bit.ly/code-beast',
-    connections: 23,
-  },
-]
+import { TQuery } from '@/src/commons/types/generated/types'
+import { FETCH_FOLLOWEES, FETCH_FOLLOWINGS } from './FollowModal.queries'
+import { useQuery } from '@apollo/client'
 
 export default function FollowModal(props: FollowModalProps) {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const { openModal } = useAuth()
 
+  const { data: followeesData } = useQuery<Pick<TQuery, 'fetchFollowees'>>(
+    FETCH_FOLLOWEES,
+    {
+      variables: { userid: props.userid as string },
+    },
+  )
+  const { data: followingsData } = useQuery<Pick<TQuery, 'fetchFollowings'>>(
+    FETCH_FOLLOWINGS,
+    {
+      variables: { userid: props.userid as string },
+    },
+  )
+
+  console.log('userId', props.userid, followeesData)
+  const followees = followeesData?.fetchFollowees.user ?? []
+  const followings = followingsData?.fetchFollowings.user ?? []
+
+  const FOLLOW_DATA = props.type === 'follower' ? followees : followings
+
   const onClickModalButton = () => {
-    if (props.isLoggedIn) {
+    console.log('로그인 됨', props.isLoggedIn)
+    if (!props.isLoggedIn) {
       // 비 로그인 시 로그인 창 오픈
       openModal('LOGIN')
     } else {
@@ -84,32 +67,38 @@ export default function FollowModal(props: FollowModalProps) {
         fontWeight="600"
         cursor="pointer"
         onClick={() => onClickModalButton()}>
-        {props.type === 'follower' ? '팔로워' : '팔로우'} 123
+        {props.type === 'follower'
+          ? `팔로워 ${props.followCount}`
+          : `팔로우 ${props.followCount}`}
       </Text>
 
       <Modal onClose={onClose} size="md" isOpen={isOpen} isCentered>
         <ModalOverlay />
-        <ModalContent>
+        <ModalContent h="400px">
           <ModalHeader mx="auto" p="12px">
             {props.type === 'follower' ? '팔로워' : '팔로우'}
           </ModalHeader>
           <ModalCloseButton />
           <Divider />
-          <ModalBody p="12px 12px 8px">
+          <ModalBody p="12px 12px 8px" overflow="auto">
             <InfiniteScroller loadMore={() => console.log('테스트')} hasMore={true}>
               <VStack>
                 {/* 테스트용 FOLLOWERS */}
-                {FOLLOWERS.map(data => (
+                {FOLLOW_DATA.map(data => (
                   <Card key={data.id} w="100%" variant="elevated" px="10px">
                     <CardBody p="0px">
                       <Flex py="6px" justify="space-between" align="center">
                         <Flex align="center">
-                          <Avatar mr="10px" name={data.name} src={data.src} />
+                          <Avatar
+                            mr="10px"
+                            name={data.nickName}
+                            src={data.picture ?? 'https://bit.ly/broken-link'}
+                          />
                           <VStack align="flex-start">
                             <Text fontSize="14px" fontWeight="600">
-                              {data.name}
+                              {data.nickName}
                             </Text>
-                            <Text fontSize="12px">{data.connections} Connections</Text>
+                            <Text fontSize="12px">?? Connections</Text>
                           </VStack>
                         </Flex>
                         <IconButton
