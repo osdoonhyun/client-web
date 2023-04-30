9ce40de7d82ce95ec246171a852aad9f82571e92
osdoonhyun <skw1996@naver.com>
2023-04-30 23:59:46 +0900

    [QCF-65] 팔로워/팔로우 쿼리, 타입 생성

--- desk/src/components/units/user/components/followModal/FollowModal.types.ts
+++ desk/src/components/units/user/components/followModal/FollowModal.types.ts
@@ -1,6 +1,29 @@
 export type FollowModalType = 'follower' | 'followee'
 
 export type FollowModalProps = {
+  userid: string
   isLoggedIn: boolean
   type: FollowModalType
+  followCount: number
+}
+
+export type FolloweesData = {
+  fetchFollowees: {
+    user: {
+      id: string
+      nickName: string
+      picture: string
+    }
+    followee: string
+  }
+}
+export type FollowingsData = {
+  fetchFollowings: {
+    user: {
+      id: string
+      nickName: string
+      picture: string
+    }
+    following: string
+  }
 }
