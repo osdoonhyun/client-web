767cb0b03c6dddeb6f322e9f4f74e7a095dca36f
Seungjin Kim <seungjin429@gmail.com>
2023-05-02 02:32:55 +0900

    [QCF-70] useAuth 리펙토링

--- desk/src/commons/hooks/useAuth.tsx
+++ desk/src/commons/hooks/useAuth.tsx
@@ -8,20 +8,23 @@ import {
 import { TAuthModalType } from '@/src/components/units/auth/Auth.types'
 import LoginIsOpen from '@/src/components/units/auth/login/Login.isOpen'
 import {
+  AUTH_EMAIL,
+  CREATE_USER,
   FETCH_LOGIN_USER,
   LOGIN,
   LOGOUT,
-  RESTORE_ACCESS_TOKEN,
+  MATCH_AUTH_NUMBER,
+  RESET_USER_PASSWORD,
   SIGNOUT,
 } from '@/src/components/units/auth/queries/mutation'
 import SignoutIsOpen from '@/src/components/units/auth/signout/Signout.isOpen'
 import SignupIsOpen from '@/src/components/units/auth/signup/Signup.isOpen'
 import { useApolloClient, useMutation } from '@apollo/client'
+import { useToast } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useRecoilState } from 'recoil'
 import { TMutation } from '../types/generated/types'
-import { useToast } from '@chakra-ui/react'
 
 export function useAuth() {
   const client = useApolloClient()
@@ -38,35 +41,65 @@ export function useAuth() {
   const [loginMutation] = useMutation<Pick<TMutation, 'login'>>(LOGIN)
   const [logoutMutation] = useMutation<Pick<TMutation, 'logOut'>>(LOGOUT)
   const [signoutMutation] = useMutation<Pick<TMutation, 'deleteUser'>>(SIGNOUT)
+  const [authEmailMutation] = useMutation<Pick<TMutation, 'authEmail'>>(AUTH_EMAIL)
+  const [matchAuthNumberMutation] =
+    useMutation<Pick<TMutation, 'matchAuthNumber'>>(MATCH_AUTH_NUMBER)
+  const [createUserMutation] = useMutation<Pick<TMutation, 'createUser'>>(CREATE_USER)
+  const [resetUserPasswordMutation] =
+    useMutation<Pick<TMutation, 'resetUserPassword'>>(RESET_USER_PASSWORD)
 
+  ////////////////////////////////////////////////////
+  // 로그인 관련 APIs
+
+  /** 유저 정보 */
   useEffect(() => {
     fetchUserInfo()
   }, [isLoggedIn])
 
-  const openModal = (type: TAuthModalType) => {
-    setAuthModalType(type)
-    setAuthModalToggle(prev => !prev)
+  /** 로그아웃 */
+  const logout = async () => {
+    await logoutMutation()
+      .then(() => {
+        clear()
+        setAuthModalType('AFTER_AUTH')
+
+        router.push('/')
+      })
+      .catch(error => {
+        if (error instanceof Error) {
+          toast({
+            title: '에러',
+            description: `${error.message}`,
+            status: 'error',
+            position: 'top',
+          })
+        }
+      })
   }
 
-  async function logout() {
-    await logoutMutation().then(() => {
-      clear()
-      setAuthModalType('AFTER_AUTH')
+  /** 회원탈퇴 */
+  const signout = async () => {
+    await signoutMutation()
+      .then(() => {
+        clear()
+        setAuthModalType('AFTER_AUTH')
 
-      router.push('/')
-    })
+        router.push('/')
+      })
+      .catch(error => {
+        if (error instanceof Error) {
+          toast({
+            title: '에러',
+            description: `${error.message}`,
+            status: 'error',
+            position: 'top',
+          })
+        }
+      })
   }
 
-  async function signout() {
-    await signoutMutation().then(() => {
-      clear()
-      setAuthModalType('AFTER_AUTH')
-
-      router.push('/')
-    })
-  }
-
-  async function login(email: string, password: string) {
+  /** 로그인 */
+  const login = async (email: string, password: string) => {
     return await loginMutation({
       variables: {
         loginInput: {
@@ -92,7 +125,8 @@ export function useAuth() {
       })
   }
 
-  async function fetchUserInfo() {
+  /** 유저 정보 */
+  const fetchUserInfo = async () => {
     if (isLoggedIn) {
       const result = await client.query({ query: FETCH_LOGIN_USER })
       const { id, email, nickName, jobGroup, provider } = result.data?.fetchLoginUser
@@ -101,11 +135,66 @@ export function useAuth() {
     }
   }
 
-  const clear = async () => {
-    setMyToken('')
-    setIsLoggedIn(false)
-    setMyUserInfo(null)
+  /** 이메일 체크 */
+  const authEmail = async (email: string, authCheck: boolean) => {
+    return await authEmailMutation({
+      variables: {
+        authEmailInput: {
+          email,
+          authCheck,
+        },
+      },
+    })
   }
+
+  /** 이메일 인증번호 체크 */
+  const matchAuthNumber = async (email: string, authNumber: string | undefined) => {
+    return await matchAuthNumberMutation({
+      variables: {
+        matchAuthInput: {
+          email,
+          authNumber,
+        },
+      },
+    })
+  }
+
+  /** 회원가입 */
+  const signin = async (email: string, password: string, jobGroup: string) => {
+    return await createUserMutation({
+      variables: {
+        createUserInput: {
+          email,
+          password,
+          jobGroup,
+        },
+      },
+    }).catch(error => {
+      if (error instanceof Error) {
+        toast({
+          title: '에러',
+          description: `${error.message}`,
+          status: 'error',
+          position: 'top',
+        })
+      }
+    })
+  }
+
+  /** 비밀번호 재설정 */
+  const resetUserPassword = async (email: string, password: string) => {
+    return await resetUserPasswordMutation({
+      variables: {
+        resetPasswordInput: {
+          email,
+          password,
+        },
+      },
+    })
+  }
+
+  ////////////////////////////////////////////////////
+  // Modal UI
 
   const loginModalUI = useCallback(() => {
     return <LoginIsOpen />
@@ -119,16 +208,31 @@ export function useAuth() {
     return <SignoutIsOpen />
   }, [])
 
+  ////////////////////////////////////////////////////
+  // Helper methods
+
+  const clear = async () => {
+    setMyToken('')
+    setIsLoggedIn(false)
+    setMyUserInfo(null)
+  }
+
+  const openModal = (type: TAuthModalType) => {
+    setAuthModalType(type)
+    setAuthModalToggle(prev => !prev)
+  }
+
   return {
     isLoggedIn,
     login,
     logout,
     signout,
+    signin,
     myUserInfo,
-    setMyUserInfo,
     myToken,
-    setMyToken,
-    fetchUserInfo,
+    authEmail,
+    matchAuthNumber,
+    resetUserPassword,
     openModal,
     LoginModalUI: loginModalUI,
     SignupModalUI: signupModalUI,
