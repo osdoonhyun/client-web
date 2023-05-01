827f2d5af4a1ecf92294d2e4d07249421a494bce
osdoonhyun <skw1996@naver.com>
2023-05-02 00:48:20 +0900

    [QCF-69] 유저 페이지 게시글/좋아요, 장비 모아보기 컴포넌트 분리 & 데이터 바인딩

--- desk/src/components/units/user/components/tabs/index.tsx
+++ desk/src/components/units/user/components/tabs/index.tsx
@@ -1,11 +1,23 @@
-import { Tabs, TabList, Tab, Icon, SimpleGrid, Box, Image } from '@chakra-ui/react'
+import {
+  Tabs,
+  TabList,
+  Tab,
+  Icon,
+  SimpleGrid,
+  Box,
+  Image,
+  Card,
+  CardBody,
+  Text,
+} from '@chakra-ui/react'
 import { MdFavorite, MdFavoriteBorder } from 'react-icons/md'
 import { BsColumnsGap } from 'react-icons/bs'
 import { AiOutlineLaptop } from 'react-icons/ai'
 import { NavigationTabsProps, UserData } from './Tabs.types'
 import ProductItem from '../productItem'
+import UserBoard from '../userBoard'
 import InfiniteScroller from '@/src/components/ui/infiniteScroller'
-import { useCallback, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import {
   FETCH_BOARDS_USER_LIKED,
@@ -30,15 +42,11 @@ export default function NavigationTabs(props: NavigationTabsProps) {
 
   const { data: userBoards } = useQuery<Pick<TQuery, 'fetchUserBoards'>>(
     FETCH_USER_BOARDS,
-    {
-      variables: { userid: props.userid as string },
-    },
+    { variables: { userid: props.userid as string } },
   )
   const { data: userLikedBoards } = useQuery<Pick<TQuery, 'fetchBoardsUserLiked'>>(
     FETCH_BOARDS_USER_LIKED,
-    {
-      variables: { userid: props.userid as string },
-    },
+    { variables: { userid: props.userid as string } },
   )
   const { data: userProducts } = useQuery<Pick<TQuery, 'fetchProducts'>>(FETCH_PRODUCTS, {
     variables: { userid: props.userid as string },
@@ -46,48 +54,46 @@ export default function NavigationTabs(props: NavigationTabsProps) {
 
   const TABS = props.isMyPage ? MY_PAGE_TAB : OTHERS_PAGE_TAB
 
-  let userData: UserData = { data: [] }
+  let userData: any
 
-  if (showUserPosts && userBoards) {
-    userData.data = userBoards
-  } else if (showLikedPosts && userLikedBoards) {
-    userData.data = userLikedBoards
-  } else if (showUserProductPosts && userProducts) {
-    userData.data = userProducts
+  if (showUserPosts) {
+    userData = userBoards?.fetchUserBoards
+  } else if (showLikedPosts) {
+    userData = userLikedBoards?.fetchBoardsUserLiked
+  } else if (showUserProductPosts) {
+    userData = userProducts?.fetchProducts
   }
 
   const handleShowUserPosts = () => {
     setShowUserPosts(true)
     setShowUserProductPosts(false)
     setShowLikedPosts(false)
-    console.log('유저 게시물 클릭')
   }
 
   const handleShowLikedPosts = () => {
     setShowUserPosts(false)
     setShowUserProductPosts(false)
     setShowLikedPosts(true)
-    console.log('좋아요 게시물 모아보기 클릭')
   }
 
   const handleShowUserProductPosts = () => {
     setShowUserPosts(false)
     setShowUserProductPosts(true)
     setShowLikedPosts(false)
-    console.log('유저 사용품 모아보기 클릭')
   }
 
-  const onClickTab = useCallback((id: number) => {
-    if (id === TabID.USER_POSTS) {
-      handleShowUserPosts()
-    } else if (id === TabID.USER_PRODUCT_POSTS) {
-      handleShowUserProductPosts()
-    } else if (id === TabID.USER_LIKED_POSTS) {
-      handleShowLikedPosts()
-    }
-  }, [])
-
-  console.log('NavigationTab', userData.data)
+  const onClickTab = useCallback(
+    (id: number) => {
+      if (id === TabID.USER_POSTS) {
+        handleShowUserPosts()
+      } else if (id === TabID.USER_PRODUCT_POSTS) {
+        handleShowUserProductPosts()
+      } else if (id === TabID.USER_LIKED_POSTS) {
+        handleShowLikedPosts()
+      }
+    },
+    [showUserPosts, showUserProductPosts, showLikedPosts],
+  )
 
   return (
     <>
@@ -108,44 +114,31 @@ export default function NavigationTabs(props: NavigationTabsProps) {
           ))}
         </TabList>
       </Tabs>
-      {/* myPage -> columns 3 / !myPage -> columns 2 */}
       {/* API 나오면 수정 작업 필요 */}
-      <SimpleGrid mt="33px" columns={3} spacing="30px">
-        <InfiniteScroller loadMore={() => console.log('테스트')} hasMore={true}>
-          {showUserPosts && (
-            <Box pos="relative">
-              <Image
-                src="https://bit.ly/dan-abramov"
-                alt="Dan Abramov"
-                bg="dGray"
-                borderRadius="10px"
-              />
-              <Box
-                pos="absolute"
-                zIndex="2"
-                top="88%"
-                left="88%"
-                _hover={
-                  props.isLiked
-                    ? undefined
-                    : {
-                        color: 'dGray.medium',
-                      }
-                }
-                color={props.isLiked ? 'dRed.400' : '#fff'}
-                onClick={props.toggleIsLiked}>
-                {props.isLiked ? (
-                  <MdFavorite size="20px" />
-                ) : (
-                  <MdFavoriteBorder size="20px" />
-                )}
-              </Box>
-            </Box>
-          )}
-          {showUserProductPosts && <ProductItem />}
-          {showLikedPosts && <ProductItem />}
-        </InfiniteScroller>
-      </SimpleGrid>
+      <InfiniteScroller loadMore={() => console.log('무한스크롤')} hasMore={true}>
+        <SimpleGrid mt="33px" columns={TABS.length} spacing="30px">
+          {userData?.map((item: any, index: number) => (
+            <React.Fragment key={index}>
+              {showUserProductPosts ? (
+                <ProductItem
+                  index={index}
+                  productId={item.id}
+                  imageUrl={item.picture}
+                  productName={item.name}
+                />
+              ) : (
+                <UserBoard
+                  index={index}
+                  boardId={item.id}
+                  imageUrl={item.pictures[0].url}
+                  isLiked={item.like}
+                  toggleIsLiked={() => console.log('test')}
+                />
+              )}
+            </React.Fragment>
+          ))}
+        </SimpleGrid>
+      </InfiniteScroller>
     </>
   )
 }
