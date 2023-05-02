8265203e06383d4fe1606b11085768ad5148f78d
yrk721 <yoorae0721@naver.com>
2023-05-02 21:23:02 +0900

    [QCF-72] props 타입 수정

--- desk/src/components/units/main/components/searchBoards/SearchBoards.types.ts
+++ desk/src/components/units/main/components/searchBoards/SearchBoards.types.ts
@@ -1,8 +1,13 @@
 import { TQuery } from '@/src/commons/types/generated/types'
+import { RefObject } from 'react'
 
 export type SearchBoardsUIProps = {
   onClickSearchBoard: (searchValue: string | undefined) => void
   onClickBoardDetail: (boardId: string) => void
   loading: boolean
   data: Pick<TQuery, 'searchBoards'> | undefined
+  searchInputRef: RefObject<HTMLInputElement>
+  isOpen: boolean
+  onOpen: () => void
+  onClose: () => void
 }
