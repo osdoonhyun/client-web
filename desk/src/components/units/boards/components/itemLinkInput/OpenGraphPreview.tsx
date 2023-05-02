84dde2f949fef2065eb2f77a2f6c448b922dbc7b
Seungjin Kim <seungjin429@gmail.com>
2023-05-03 03:08:57 +0900

    [QCF-35] OpenGraph Preview UI 개발

---
+++ desk/src/components/units/boards/components/itemLinkInput/OpenGraphPreview.tsx
@@ -0,0 +1,41 @@
+import { HStack, Image, Text, VStack, useColorModeValue } from '@chakra-ui/react'
+import { OpenGraphPreviewProps } from './types'
+
+export default function OpenGraphPreview(props: OpenGraphPreviewProps) {
+  return (
+    <HStack justify={'stretch'}>
+      {Object.keys(props.item.og).length !== 0 && (
+        <HStack
+          justify={'stretch'}
+          w={'100%'}
+          h={'100px'}
+          borderRadius={'6px'}
+          border={'1px solid'}
+          color={useColorModeValue('dBlack', 'dGray.light')}
+          borderColor={useColorModeValue('gray.200', '#bababa1e')}>
+          <VStack align={'flex-start'} width={'70%'} p={'16px'}>
+            <Text fontSize={'16px'} fontWeight={500}>
+              {props.item.og.name}
+            </Text>
+            <Text
+              fontSize={'14px'}
+              color={useColorModeValue('dGray.dark', 'dGray.medium')}>
+              {props.item.og.description}
+            </Text>
+            <Text fontSize={'14px'} pt={'8px'}>
+              {props.item.og.url}
+            </Text>
+          </VStack>
+          <Image
+            height={'98%'}
+            width={'30%'}
+            objectFit={'cover'}
+            src={props.item.og.imageUrl ?? ''}
+            borderRadius="lg"
+            bgGradient="linear(to-l, #7928CA, #FF0080)"
+          />
+        </HStack>
+      )}
+    </HStack>
+  )
+}
