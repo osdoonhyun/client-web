793898b3f02eae2b91256edfcaf9680b507601ae
yrk721 <yoorae0721@naver.com>
2023-05-02 16:18:38 +0900

    [QCF-66] 작성자 아바타 이미지/userId 타입 추가

--- desk/src/components/units/main/components/mainBoardSlider/types.ts
+++ desk/src/components/units/main/components/mainBoardSlider/types.ts
@@ -2,6 +2,7 @@ export type MainBoardSliderProps = {
   images: string[]
   titles: string[]
   writers: string[]
+  writerImages: (string | null | undefined)[]
   boardIds: string[]
   userIds: string[]
   children?: React.ReactNode
