21eae8326070b78d67e7bcb06dad9dc63773f9c6
applepykim <applepykim@naver.com>
2023-05-03 17:48:34 +0900

    [QCF-78] <유저인증 / 로그인> 완료 / 마지막 로그인 표시 > localStorage 이용 할려고 다시 재설치 하였습니다. recoil-persist

--- desk/src/commons/store/atom.ts
+++ desk/src/commons/store/atom.ts
@@ -1,10 +1,16 @@
 import { TAuthModalType } from '@/src/components/units/auth/Auth.types'
 import { atom } from 'recoil'
 import { TUser } from '../types/generated/types'
+import { recoilPersist } from 'recoil-persist'
+
+const { persistAtom: local } = recoilPersist({
+  key: `atomLocal`,
+})
 
 export const MyEmailSave = atom({
   key: `myEmailSave`,
   default: '',
+  effects_UNSTABLE: [local],
 })
 
 export const MyToken = atom<string>({
