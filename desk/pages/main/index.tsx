2d2f6a9f37ea013cabab205239d8af0de9d0b5fd
yrk721 <yoorae0721@naver.com>
2023-05-05 19:39:51 +0900

    [QCF-68] 좋아요 카테고리 useAuth 적용

--- desk/pages/main/index.tsx
+++ desk/pages/main/index.tsx
@@ -1,8 +1,8 @@
 import AllProducts from '@/src/components/units/main/categories/allProducts/AllProducts.container'
 import Best from '@/src/components/units/main/categories/best/Best.container'
 import Recent from '@/src/components/units/main/categories/recent/Recent.container'
-import Like from '@/src/components/units/main/categories/like/Like.container'
 import FollowingBoards from '@/src/components/units/main/categories/followingBoards/followingBoards.container'
+import Like from '@/src/components/units/main/categories/like/Like.container'
 import Youtube from '@/src/components/units/main/categories/youtube/Youtube.container'
 import BoardsRegisterButton from '@/src/components/units/main/components/boardsRegisterButton'
 import { useAuth } from '@/src/commons/hooks/useAuth'
@@ -21,15 +21,14 @@ export default function MainPage() {
     <>
       <Best />
       <Recent />
-      {/* 좋아요 한 게시물은 추가 예정 */}
-      {/* {isLoggedIn && (
-        <div onClick={onClickUnAuth}>
-          <Like />
-        </div>
-      )} */}
       {isLoggedIn && (
         <div onClick={onClickUnAuth}>
           <FollowingBoards />
+        </div>
+      )}
+      {isLoggedIn && (
+        <div onClick={onClickUnAuth}>
+          <Like />
         </div>
       )}
       <Youtube />
