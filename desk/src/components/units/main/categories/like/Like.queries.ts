b30f44f7a9d451ac8e53088ec1989eeafad2a13f
yrk721 <yoorae0721@naver.com>
2023-05-05 19:29:42 +0900

    [QCF-68] 메인페이지 좋아요 카테고리 쿼리 / 타입 수정

---
+++ desk/src/components/units/main/categories/like/Like.queries.ts
@@ -0,0 +1,25 @@
+import { gql } from '@apollo/client'
+
+export const FETCH_BOARDS_USER_LIKED = gql`
+  query FetchBoardsUserLiked {
+    fetchBoardsUserLiked {
+      id
+      title
+      writer {
+        id
+        email
+        nickName
+        picture
+      }
+      pictures {
+        id
+        url
+        isMain
+      }
+      like
+      hashtags {
+        hashtag
+      }
+    }
+  }
+`
