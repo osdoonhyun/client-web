1d97b68a7ed552f6eefc07b4c28633d30f7b8dde
yrk721 <yoorae0721@naver.com>
2023-05-06 19:56:49 +0900

    [QCF-68] 좋아요 컴포넌트 useAuth 적용 - 비로그인 시 좋아요 버튼 클릭 시 로그인 모달 렌더링

--- desk/src/components/units/main/components/LikeButton/index.tsx
+++ desk/src/components/units/main/components/LikeButton/index.tsx
@@ -4,59 +4,59 @@ import {
 } from '@/src/commons/types/generated/types'
 import { useMutation } from '@apollo/client'
 import { UPDATE_BOARD_LIKER } from './LiksButton.queries'
-import { LikeButtonProps } from './LiksButton.types'
 import { MdFavorite, MdFavoriteBorder } from 'react-icons/md'
 import { Box } from '@chakra-ui/react'
 import { useState } from 'react'
 import { FETCH_BOARDS } from '../../categories/recent/Recent.queries'
+import { LikeButtonProps } from './LiksButton.types'
+import { useAuth } from '@/src/commons/hooks/useAuth'
 
 export default function LikeButton(props: LikeButtonProps) {
   const [isLiked, setIsLiked] = useState(props.isLiked)
+  const { currentUser, isLoggedIn, openModal } = useAuth()
 
   const [updateBoardLiker] = useMutation<
     Pick<TMutation, 'updateBoardLiker'>,
     TMutationUpdateBoardLikerArgs
   >(UPDATE_BOARD_LIKER)
 
-  const onClickLikeButton = async (
-    e: React.MouseEvent<HTMLDivElement>,
-    boardid: string,
-  ) => {
+  const onClickLikeButton = async (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation()
-    console.log('boardid', boardid)
-    await updateBoardLiker({
-      variables: {
-        boardid,
-      },
-      refetchQueries: [{ query: FETCH_BOARDS }],
-    }).then(result => {
-      console.log('result', result)
-      if (result.data) {
-        setIsLiked(result.data.updateBoardLiker)
-      } else {
-        console.error('좋아요 업데이트에 실패했습니다.')
-      }
-    })
+    if (!isLoggedIn) {
+      openModal('LOGIN')
+    } else {
+      // 로그인 상태일 때만 API 호출
+      await updateBoardLiker({
+        variables: {
+          boardid: props.boardId,
+        },
+        refetchQueries: [{ query: FETCH_BOARDS }],
+      }).then(result => {
+        if (result.data) {
+          setIsLiked(result.data.updateBoardLiker)
+        } else {
+          console.error('좋아요 업데이트에 실패했습니다.')
+        }
+      })
+    }
   }
 
   return (
-    <>
-      <Box
-        pos="absolute"
-        zIndex="2"
-        top="85%"
-        left="88%"
-        _hover={
-          isLiked
-            ? undefined
-            : {
-                color: 'dGray.medium',
-              }
-        }
-        color={isLiked ? 'dRed.400' : '#fff'}
-        onClick={e => onClickLikeButton(e, props.boardId)}>
-        {isLiked ? <MdFavorite size="20px" /> : <MdFavoriteBorder size="20px" />}
-      </Box>
-    </>
+    <Box
+      pos="absolute"
+      zIndex="2"
+      top="85%"
+      left="88%"
+      _hover={
+        isLiked
+          ? undefined
+          : {
+              color: 'dGray.medium',
+            }
+      }
+      color={isLiked ? 'dRed.400' : '#fff'}
+      onClick={onClickLikeButton}>
+      {isLiked ? <MdFavorite size="20px" /> : <MdFavoriteBorder size="20px" />}
+    </Box>
   )
 }
