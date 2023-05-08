240f42c25746fe7fed815fdd008dbea2621a8dab
applepykim <applepykim@naver.com>
2023-05-08 15:24:25 +0900

    [QCF-86] <유저인증 / 추가개선> 완료 / 프로필에 닉네임 뜨기

--- desk/src/components/units/main/viewMore/recentMore/RecentMore.presenter.tsx
+++ desk/src/components/units/main/viewMore/recentMore/RecentMore.presenter.tsx
@@ -82,12 +82,7 @@ export default function RecentMoreUI(props: RecentMoreUIProps) {
                     mt={2}
                     cursor="pointer"
                     onClick={() => props.onClickUserDetail(board.writer.id)}>
-                    <Avatar
-                      w="20px"
-                      h="20px"
-                      mr="5px"
-                      src={board.writer.picture || 'https://bit.ly/broken-link'}
-                    />
+                    <Avatar w="20px" h="20px" mr="5px" src={board.writer.picture || ''} />
                     {board.writer.nickName}
                   </Text>
                 </Center>
