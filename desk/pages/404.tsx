9dd72ca30e0b6bf5585092dbc431c0e4eb064c28
yrk721 <yoorae0721@naver.com>
2023-05-13 07:39:46 +0900

    [QCF-93] 에러페이지 UI 수정 - 컬러 변경

--- desk/pages/404.tsx
+++ desk/pages/404.tsx
@@ -1,29 +1,34 @@
-import { Box, Button, Heading, Text } from '@chakra-ui/react'
+import { Box, Button, Heading, Text, useColorModeValue } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 
 export default function Error404Page() {
   const router = useRouter()
 
   return (
-    <Box textAlign="center" py={10} px={6} mt={50}>
+    <Box textAlign="center" py={10} px={6} mt={100}>
       <Heading
         display="inline-block"
         as="h1"
         size="3xl"
-        bgGradient="linear(to-r, blue.400, blue.600)"
+        bgGradient="linear(to-r, #9397ff, dPrimary)"
         backgroundClip="text">
         404
       </Heading>
-      <Text fontSize="2xl" mt={3} mb={2}>
+      <Text
+        mt={3}
+        mb={2}
+        fontSize="2xl"
+        fontWeight={700}
+        color={useColorModeValue('gray.500', 'dGray.medium')}>
         Page Not Found
       </Text>
-      <Text color={'gray.500'} mb={6}>
-        오류가 발생했습니다.
+      <Text mb={6} color={'gray.500'} fontWeight={500}>
+        오류가 발생했습니다. 다시 시도해 주세요.
       </Text>
       <Button
         mt={'30px'}
         colorScheme="blue"
-        bgGradient="linear(to-r, blue.400, blue.500, blue.600)"
+        bgGradient="linear(to-r, #9397ff, dPrimary,dPrimaryHover.darker)"
         color="white"
         variant="solid"
         onClick={() => router.push('/')}>
