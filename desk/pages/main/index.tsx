c2bf3d440dd441098d58fc3ed6b14dbc7a4e3c86
yrk721 <yoorae0721@naver.com>
2023-05-18 05:08:36 +0900

    [QCF-93] 메인페이지에 카테고리 추가

--- desk/pages/main/index.tsx
+++ desk/pages/main/index.tsx
@@ -1,9 +1,10 @@
-import AllProducts from '@/src/components/units/main/categories/allProducts/AllProducts.container'
 import Best from '@/src/components/units/main/categories/best/Best.container'
 import Recent from '@/src/components/units/main/categories/recent/Recent.container'
 import FollowingBoards from '@/src/components/units/main/categories/followingBoards/followingBoards.container'
 import Like from '@/src/components/units/main/categories/like/Like.container'
+import JobGroup from '@/src/components/units/main/categories/jobGroup/JobGroup.container'
 import Youtube from '@/src/components/units/main/categories/youtube/Youtube.container'
+import AllProducts from '@/src/components/units/main/categories/allProducts/AllProducts.container'
 import BoardsRegisterButton from '@/src/components/units/main/components/boardsRegisterButton'
 import { useAuth } from '@/src/commons/hooks/useAuth'
 
@@ -16,6 +17,7 @@ export default function MainPage() {
       <Recent />
       {isLoggedIn && <FollowingBoards />}
       {isLoggedIn && <Like />}
+      <JobGroup />
       <Youtube />
       <AllProducts />
       <BoardsRegisterButton />
