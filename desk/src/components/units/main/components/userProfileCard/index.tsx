305b2c831209a3894083fdf069ac1ef4fa79e455
yrk721 <yoorae0721@naver.com>
2023-05-18 18:27:42 +0900

    [QCF-93] 프로필카드 모바일처리 추가

--- desk/src/components/units/main/components/userProfileCard/index.tsx
+++ desk/src/components/units/main/components/userProfileCard/index.tsx
@@ -5,85 +5,88 @@ import {
   Center,
   Box,
   VStack,
-  useColorModeValue,
   Avatar,
+  useColorModeValue,
+  useBreakpointValue,
 } from '@chakra-ui/react'
 import { ProfileCardProps } from './types'
 
 export default function ProfileCard(props: ProfileCardProps) {
+  const cardWidth = useBreakpointValue({ base: '100%', md: '250px' })
+  const cardHeight = useBreakpointValue({ base: 'auto', md: '295px' })
+
   return (
-    <>
-      <VStack>
-        <Flex my={3} justify={'flex-start'} align={'center'} wrap={'wrap'}>
-          <Card
-            w="250px"
-            h="295px"
-            cursor="pointer"
-            bgColor={useColorModeValue('dGray.light', '#a0a0a01e')}
-            _hover={{
-              borderWidth: '2px',
-              borderColor: 'dPrimaryHover.transparency',
-              transform: 'scale(1.03)',
-            }}
-            onClick={props.onClick}>
-            <CardBody borderRadius="lg">
-              <Center mt={'20px'}>
-                <Avatar
-                  size={'xl'}
-                  name={props.nickName}
-                  src={props.image ?? 'https://bit.ly/broken-link'}
-                />
-              </Center>
-              <Flex flexDirection={'column'} mt={5}>
-                <Center>
-                  <Center
-                    p={'2px 8px 1px 8px'}
-                    w="fit-content"
-                    bg="dPrimary"
-                    color="white"
-                    borderRadius={'4px'}
-                    fontWeight="700"
-                    letterSpacing={1}
-                    fontSize={{ base: 'xs', md: 'xs' }}>
-                    {props.jobGroup}
-                  </Center>
-                </Center>
+    <VStack>
+      <Flex my={3} justify={'flex-start'} align={'center'} wrap={'wrap'}>
+        <Card
+          w={cardWidth}
+          h={cardHeight}
+          cursor="pointer"
+          bgColor={useColorModeValue('dGray.light', '#a0a0a01e')}
+          _hover={{
+            borderWidth: '2px',
+            borderColor: 'dPrimaryHover.transparency',
+            transform: 'scale(1.03)',
+          }}
+          onClick={props.onClick}>
+          <CardBody borderRadius="lg">
+            <Center mt={'20px'}>
+              <Avatar
+                size={{ base: 'lg', md: 'xl' }}
+                name={props.nickName}
+                src={props.image ?? 'https://bit.ly/broken-link'}
+              />
+            </Center>
+            <Flex flexDirection={'column'} mt={5}>
+              <Center>
                 <Center
-                  mt={4}
-                  w="100%"
+                  p={'2px 8px 1px 8px'}
+                  w="fit-content"
+                  bg="dPrimary"
+                  color="white"
+                  borderRadius={'4px'}
                   fontWeight="700"
-                  fontSize={{ base: 'sm', md: '13pt' }}
-                  color={useColorModeValue('dGray.dark', 'dGray.light')}>
-                  {props.nickName}
+                  letterSpacing={1}
+                  fontSize={{ base: '8pt', md: 'xs' }}>
+                  {props.jobGroup}
                 </Center>
-                <Center
-                  mt={4}
-                  fontWeight="600"
-                  fontSize={{ base: 'sm', md: 'md' }}
-                  color={useColorModeValue('dGray.dark', 'dGray.light')}>
-                  <Flex
-                    justifyContent="space-around"
-                    color={useColorModeValue('#232323d5', 'dGray.light')}>
-                    <Center mx={4}>
-                      팔로우
-                      <Box ml={1} fontWeight="800">
-                        {props.followeesCount}
-                      </Box>
-                    </Center>
-                    <Center mx={4}>
-                      팔로잉
-                      <Box ml={1} fontWeight="800">
-                        {props.followingsCount}
-                      </Box>
-                      <br />
-                    </Center>
-                  </Flex>
-                </Center>
-              </Flex>
-            </CardBody>
-          </Card>
-        </Flex>
-      </VStack>
-    </>
+              </Center>
+              <Center
+                mt={4}
+                w="100%"
+                fontWeight="700"
+                fontSize={{ base: 'sm', md: '13pt' }}
+                color={useColorModeValue('dGray.dark', 'dGray.light')}>
+                {props.nickName}
+              </Center>
+              <Center
+                mt={4}
+                w={'100%'}
+                fontWeight="600"
+                fontSize={{ base: 'xs', md: 'md' }}
+                color={useColorModeValue('dGray.dark', 'dGray.light')}>
+                <Flex
+                  justifyContent="space-around"
+                  color={useColorModeValue('#232323d5', 'dGray.light')}>
+                  <Center mx={3}>
+                    팔로우
+                    <Box ml={1} fontWeight="800">
+                      {props.followeesCount}
+                    </Box>
+                  </Center>
+                  <Center mx={3}>
+                    팔로잉
+                    <Box ml={1} fontWeight="800">
+                      {props.followingsCount}
+                    </Box>
+                    <br />
+                  </Center>
+                </Flex>
+              </Center>
+            </Flex>
+          </CardBody>
+        </Card>
+      </Flex>
+    </VStack>
   )
 }
