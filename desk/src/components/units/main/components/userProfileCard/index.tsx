06f92b04b9e244a9d3136a112d0f3a0e3d83d85d
yrk721 <yoorae0721@naver.com>
2023-05-18 15:18:15 +0900

    [QCF-93] 유저 프로필 카드 직군 추가

--- desk/src/components/units/main/components/userProfileCard/index.tsx
+++ desk/src/components/units/main/components/userProfileCard/index.tsx
@@ -17,7 +17,7 @@ export default function ProfileCard(props: ProfileCardProps) {
         <Flex my={3} justify={'flex-start'} align={'center'} wrap={'wrap'}>
           <Card
             w="250px"
-            h="290px"
+            h="295px"
             cursor="pointer"
             bgColor={useColorModeValue('dGray.light', '#a0a0a01e')}
             _hover={{
@@ -27,15 +27,29 @@ export default function ProfileCard(props: ProfileCardProps) {
             }}
             onClick={props.onClick}>
             <CardBody borderRadius="lg">
-              <Center mt={'30px'}>
+              <Center mt={'20px'}>
                 <Avatar
                   size={'xl'}
                   name={props.nickName}
                   src={props.image ?? 'https://bit.ly/broken-link'}
                 />
               </Center>
-              <Flex flexDirection={'column'} mt={6}>
+              <Flex flexDirection={'column'} mt={5}>
+                <Center>
+                  <Center
+                    p={'2px 8px 1px 8px'}
+                    w="fit-content"
+                    bg="dPrimary"
+                    color="white"
+                    borderRadius={'4px'}
+                    fontWeight="700"
+                    letterSpacing={1}
+                    fontSize={{ base: 'xs', md: 'xs' }}>
+                    {props.jobGroup}
+                  </Center>
+                </Center>
                 <Center
+                  mt={4}
                   w="100%"
                   fontWeight="700"
                   fontSize={{ base: 'sm', md: '13pt' }}
@@ -65,9 +79,6 @@ export default function ProfileCard(props: ProfileCardProps) {
                     </Center>
                   </Flex>
                 </Center>
-                <Box ml={1} fontWeight="800">
-                  {props.jobGroup}
-                </Box>
               </Flex>
             </CardBody>
           </Card>
