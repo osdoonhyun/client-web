d38d17d8d2ecda67d05f2938d63325bacd918d54
yrk721 <yoorae0721@naver.com>
2023-05-18 15:21:07 +0900

    [QCF-93] jobGroupMapping 추가, 전체 유저 조회 추가

--- desk/src/components/units/main/viewMore/jobGroupMore/JobGroupMore.container.tsx
+++ desk/src/components/units/main/viewMore/jobGroupMore/JobGroupMore.container.tsx
@@ -2,6 +2,7 @@ import JobGroupMoreUI from './JobGroupMore.presenter'
 import { TQuery } from '@/src/commons/types/generated/types'
 import { useQuery } from '@apollo/client'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { FETCH_USERS } from './JobGroupMore.queries'
 import CustomSpinner from '@/src/components/ui/customSpinner'
 import ErrorMessage from '@/src/components/ui/errorMessage'
@@ -9,10 +10,48 @@ import { Center } from '@chakra-ui/react'
 
 export default function JobGroupMore() {
   const { data, loading, error } = useQuery<Pick<TQuery, 'fetchUsers'>>(FETCH_USERS)
-
   const router = useRouter()
 
-  const JOB_GROUP = ['IT', '마케팅 / 광고', '디자인', '미디어', '교육', '학생', '기타']
+  const JOB_GROUP = [
+    'IT',
+    '마케팅 / 광고',
+    '디자인',
+    '미디어',
+    '교육',
+    '학생',
+    '기타',
+    '전체',
+  ]
+  const [selectedJobGroup, setSelectedJobGroup] = useState(JOB_GROUP[0])
+
+  const jobGroupMapping: Record<string, string> = {
+    '마케팅 / 광고': '마케팅',
+    디자인: '디자인',
+    미디어: '미디어',
+    교육: '교육',
+    학생: '학생',
+    개발자: 'IT',
+    IT: 'IT',
+    기타: '기타',
+    미선택: '기타',
+  }
+
+  const filteredUsers =
+    selectedJobGroup === '전체'
+      ? data?.fetchUsers
+      : data?.fetchUsers.filter(
+          user =>
+            (jobGroupMapping[user.jobGroup] || user.jobGroup) ===
+            jobGroupMapping[selectedJobGroup],
+        )
+
+  const handleOnLoadMore = () => {
+    console.log('더보기')
+  }
+
+  const onClickUserDetail = (userId: string) => {
+    router.push(`/${userId}`)
+  }
 
   if (loading) {
     return (
@@ -27,24 +66,13 @@ export default function JobGroupMore() {
     return <ErrorMessage message={error.message} />
   }
 
-  const userData = data?.fetchUsers
-  if (!userData || userData.length === 0) {
-    return <p>해당 직군에 등록된 유저 정보가 없습니다.</p>
-  }
-
-  const handleOnLoadMore = () => {
-    console.log('더보기')
-  }
-
-  const onClickUserDetail = (userId: string) => {
-    router.push(`/${userId}`)
-  }
-
   return (
     <>
       <JobGroupMoreUI
         jobGroupData={JOB_GROUP}
-        userData={data.fetchUsers}
+        selectedJobGroup={selectedJobGroup}
+        onJobGroupChange={setSelectedJobGroup}
+        userData={filteredUsers}
         onLoadMore={handleOnLoadMore}
         onClickUserDetail={onClickUserDetail}
       />
